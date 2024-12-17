94f8c9837f9aa2a621bd96ad9acc6264e0595470
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2024-12-17 14:41:45 -0500

    First commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![deny(
+    warnings,
+    clippy::all,
+    clippy::cargo,
+    clippy::pedantic,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_must_use
+)]
+#![allow(clippy::cargo)] // FIXME
+#![allow(
+    clippy::items_after_statements,
+    clippy::missing_errors_doc,
+    clippy::semicolon_if_nothing_returned,
+    clippy::struct_excessive_bools,
+    clippy::let_underscore_untyped
+)]
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_variables))]
+
+fn main() {
+    println!("Hello, world!");
+}
