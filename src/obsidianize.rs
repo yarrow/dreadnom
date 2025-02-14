fc212b0534116e45606d3f2d5f7364b690fb6206
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2025-02-13 21:31:32 -0500

    Move guts of main() to reformat_for_obsidian
    ... in module obsidianize

---
+++ src/obsidianize.rs
@@ -0,0 +1,162 @@
+use std::{fs, fs::File, io::Write, str, str::FromStr, sync::LazyLock};
+
+use anyhow::{bail, Context, Result};
+use camino::{Utf8Path, Utf8PathBuf};
+use regex::bytes::Regex;
+
+use crate::parse::{name_prologue_body, parse};
+
+pub fn reformat_for_obsidian(source: &Utf8PathBuf, obsidian: &Utf8PathBuf) -> Result<()> {
+    const PRE_PROLOGUE: &[u8] = b"---\nobsidianUIMode: preview\n---\n\n";
+
+    const TXT: &str = ".txt";
+
+    // Get the names of the source files
+    let article_names = gather_and_validate_visible_files_in(source, TXT)?;
+    if article_names.is_empty() {
+        bail!("No articles found in directory {source}");
+    } else if let Some(unnumbered) =
+        article_names.iter().find(|&a| number_and_name_from(a).0.is_none())
+    {
+        bail!(
+            "All articles must start with a number, but found {unnumbered} in directory {source}"
+        );
+    }
+
+    // Ensure that `obsdian` exists and contains only `.md` files (or ignored files)
+    if obsidian.read_dir_utf8().is_err() {
+        fs::create_dir(obsidian).with_context(|| format!("Can't create directory {obsidian}"))?;
+    }
+    // For `obsidian` we don't need the files, just the validation
+    gather_and_validate_visible_files_in(obsidian, ".md")?;
+
+    // Create a .md file in `obsidian` for each `.txt` file in `source`
+    for txt_name in article_names {
+        if txt_name.ends_with(" copy.txt") {
+            // This avoids a duplicate file in Thingonomicon
+            continue;
+        }
+        let source_path = source.join(&txt_name);
+        let original = fs::read_to_string(&source_path)?;
+
+        let special_case;
+        let (content_name, prologue, to_be_parsed) = match urban_idea_special_case(&original) {
+            Some((name, parseable)) => {
+                special_case = parseable;
+                (name, String::new(), &special_case[..])
+            }
+            None => name_prologue_body(&original)
+                .with_context(|| format!("Can't understand file {source_path}"))?,
+        };
+
+        let (Some(n), fs_name) = number_and_name_from(&txt_name) else {
+            bail!("This can't happen: all article_names start with a number");
+        };
+        let (_, content_name) = number_and_name_from(&content_name);
+        let description = if n == 12 {
+            // `content_name` is correct for the two `12*` files in the Thingonomicon
+            // and (as it happens) for the one `12*` files in the Laironomicon
+            content_name
+        } else if fs_name.len() > content_name.len() {
+            fs_name
+        } else {
+            content_name
+        };
+
+        // Currently there's only one file with a number >= 100; we choose to
+        // let that one sort to the end without a number rather than use three digits.
+        let output_name = if n < 100 { format!("{n:02} {description}") } else { description };
+
+        let mut body = prologue;
+        let parsed = parse(&output_name, to_be_parsed)
+            .with_context(|| format!("Can't understand file {source_path}"))?;
+        body.push_str(&parsed.to_string());
+
+        let output_path = obsidian.join(&output_name).with_extension("md");
+        let mut output = File::create(&output_path)?;
+        output.write_all(PRE_PROLOGUE)?;
+        output.write_all(body.as_bytes())?
+    }
+
+    Ok(())
+}
+
+fn gather_and_validate_visible_files_in(dir: &Utf8Path, extension: &str) -> Result<Vec<String>> {
+    let entries = dir.read_dir_utf8().with_context(|| format!("Can't open directory {dir}"))?;
+    let mut names = Vec::new();
+    for entry in entries {
+        let entry = entry.with_context(|| "Error reading directory {dir}")?;
+        let path = entry.path();
+        let meta = fs::metadata(path).with_context(|| "Error reading {path}")?;
+        if meta.is_file() {
+            let Some(name) = path.file_name() else { continue };
+            if !(name.starts_with('.')) {
+                let name = name.to_string();
+                if !name.ends_with(extension) {
+                    bail!("Files in {dir} should end in {extension} but found {name}");
+                }
+
+                names.push(name.to_string());
+            }
+        }
+    }
+    Ok(names)
+}
+
+fn urban_idea_special_case(contents: &str) -> Option<(String, String)> {
+    static URBAN: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"^#\s+71:? Urban.*\n#ideas\s*(1.)").unwrap());
+
+    if let Some(urb) = URBAN.captures(contents.as_bytes()) {
+        let start = urb.get(1).unwrap().start();
+        return Some((
+            "71 Urban Events".to_string(),
+            ["\n## Ideas\n", &contents[start..]].concat(),
+        ));
+    }
+    None
+}
+
+fn number_and_name_from(name: &str) -> (Option<u32>, String) {
+    static PARTS: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"^(\d+)?[\s_]*(.*?)(?:.txt)?$").unwrap());
+    let Some(cap) = PARTS.captures(name.as_bytes()) else {
+        return (None, name.to_string());
+    };
+    let n = match cap.get(1) {
+        Some(n_bytes) => {
+            let n_str = str::from_utf8(n_bytes.as_bytes()).unwrap();
+            Some(u32::from_str(n_str).unwrap())
+        }
+        None => None,
+    };
+    (n, String::from_utf8(cap[2].to_vec()).unwrap())
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn special_case_for_urban_ideas() {
+        let prologue1 = "# 71 Urban\n#ideas\n";
+        let prologue2 = "# 71: Urban Cities\n#ideas\n\n\n";
+        let body = "1. blah blah\n 2.blah diddy blah\n";
+        for prologue in [prologue1, prologue2] {
+            let contents = [prologue, body].concat();
+            assert_eq!(
+                urban_idea_special_case(&contents).unwrap(),
+                ("71 Urban Events".to_string(), ["\n## Ideas\n", body].concat())
+            );
+        }
+    }
+
+    #[test]
+    fn number_and_name_from_splits_initial_number_from_rest() {
+        let a = "12_stuff.txt";
+        let b = "12 stuff";
+        let c = "stuff.txt";
+        assert_eq!(number_and_name_from(a), (Some(12), "stuff".to_string()));
+        assert_eq!(number_and_name_from(b), (Some(12), "stuff".to_string()));
+        assert_eq!(number_and_name_from(c), (None, "stuff".to_string()));
+    }
+}
---
+++ src/parse.rs
@@ -0,0 +1,374 @@
+#![allow(clippy::bool_to_int_with_if, clippy::enum_glob_use)]
+
+use anyhow::{self, bail, Context, Result};
+use logos::Logos;
+use regex::Regex;
+use std::{error, fmt, ops::Range, str, sync::LazyLock};
+
+pub(crate) fn name_prologue_body(contents: &str) -> Result<(String, String, &str)> {
+    static SUBHEAD: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\n#+\s").unwrap());
+    const COPYRIGHT: &str = "©";
+    static OGL: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^(?:Include )?OGL\b").unwrap());
+
+    let file_name = embedded_file_name(contents)?;
+
+    let Some(newline) = contents.find('\n') else {
+        return Ok((file_name, String::new(), ""));
+    };
+
+    let mut proem = &contents[newline..];
+    let remainder;
+    if let Some(sub) = SUBHEAD.find(proem) {
+        remainder = &proem[sub.start()..];
+        let pro_start = if sub.start() == 0 { 0 } else { 1 }; // Skip the leading '\n'
+        proem = &proem[pro_start..sub.start()];
+    } else {
+        proem = &proem[1..];
+        remainder = "";
+    }
+
+    let mut prologue = Vec::new();
+    if remainder.is_empty() {
+        prologue.push(proem.to_owned());
+    } else {
+        let mut lines = proem.lines();
+        for line in lines.by_ref() {
+            if line.contains(COPYRIGHT) || OGL.is_match(line) {
+                prologue.push(line.to_owned());
+                prologue.push("\n".to_owned());
+            }
+        }
+        if prologue.is_empty() {
+            bail!("It doesn't contain a copyright symbol ({COPYRIGHT})");
+        }
+    }
+
+    Ok((file_name, prologue.concat(), remainder))
+}
+
+fn embedded_file_name(contents: &str) -> Result<String> {
+    static HEADER: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^#+\s+(.*\S)\s*").unwrap());
+    static THINGS_20: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"^(?:20 Things #|Monstrous Lair #)(.*)").unwrap());
+    static COLON: LazyLock<Regex> = LazyLock::new(|| Regex::new(r":").unwrap());
+
+    let Some(header_caps) = HEADER.captures(contents) else {
+        bail!("It doesn't start with a Markdown header");
+    };
+    let initial_file_name = header_caps[1].trim();
+    let mut file_name = match THINGS_20.captures(initial_file_name) {
+        Some(caps) => caps[1].trim().to_string(),
+        None => initial_file_name.trim().to_string(),
+    };
+    if &file_name == "Name" {
+        static FROM_COPYRIGHT_LINE: LazyLock<Regex> =
+            LazyLock::new(|| Regex::new(r"\n[^#]+#\d\d:\s*([^.]+)\.\s*©").unwrap());
+        if let Some(found) = FROM_COPYRIGHT_LINE.captures(contents) {
+            file_name = found[1].to_string();
+        }
+    }
+
+    Ok(COLON.replace(&file_name, "").to_string())
+}
+
+#[derive(Default, Debug, Clone, PartialEq)]
+enum ThisCantHappen {
+    #[default]
+    UnexpectedParsingError,
+}
+
+impl error::Error for ThisCantHappen {}
+impl fmt::Display for ThisCantHappen {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Internal error: Unexpected Parsing Error")
+    }
+}
+#[derive(Debug, Logos, PartialEq)]
+#[logos(error = ThisCantHappen)]
+enum LineKind {
+    #[regex("\n\\d+\\.[^\n]*")]
+    ListItem,
+
+    #[regex("\n#+ [^\n]*")]
+    Header,
+
+    #[regex("\n[^\n]*")]
+    Vanilla,
+}
+
+struct ParseParts<'a> {
+    name: &'a str,
+    parts: Vec<String>,
+    link: String,
+}
+
+impl<'a> ParseParts<'a> {
+    fn new(name: &'a str, link: &str) -> Self {
+        Self { name, parts: Vec::new(), link: link.to_string() }
+    }
+    fn push(&mut self, text: &str) {
+        self.parts.push(text.to_string());
+    }
+    fn push_with_paragraph(&mut self, text: String) {
+        const PARAGRAPH: &str = "\n\n";
+        self.push(PARAGRAPH);
+        self.parts.push(text);
+        self.push(PARAGRAPH);
+    }
+    fn set_link(&mut self, link_text: &str) {
+        self.link = make_link(link_text);
+    }
+    fn push_link(&mut self) {
+        self.push_with_paragraph(self.link.clone());
+    }
+    fn push_dice_code(&mut self) {
+        self.push_with_paragraph(dice_code(self.name, &self.link));
+    }
+    fn concat(&self) -> String {
+        self.parts.concat()
+    }
+}
+
+pub(crate) fn parse(name: &str, contents: &str) -> Result<String> {
+    use LineKind::*;
+
+    if contents.is_empty() {
+        return Ok(String::new());
+    }
+    if !contents.starts_with('\n') {
+        bail!(r"Internal error: `parse(contents)` requires `contents` to start with a newline");
+    }
+
+    let mut parts = ParseParts::new(name, "^START");
+    let (mut start, mut end) = (0, 0);
+    let mut previous = Vanilla;
+
+    for (kind, span) in LineKind::lexer(contents).spanned() {
+        let kind = kind.with_context(|| format!("Seen so far: {:?}", parts.concat()))?;
+        if kind == previous {
+            end = span.end;
+        } else {
+            parts.push(&contents[start..end]);
+            if previous == ListItem {
+                parts.push_link();
+            }
+            Range { start, end } = span;
+            match kind {
+                ListItem => parts.push_dice_code(),
+                Header => parts.set_link(&contents[span]),
+                Vanilla => {}
+            }
+        }
+        previous = kind;
+    }
+    parts.push(&contents[start..end]);
+    if previous == ListItem {
+        parts.push_link();
+    }
+
+    static EXTRA_NEWLINES: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\n\n\n+").unwrap());
+    Ok(EXTRA_NEWLINES.replace_all(&parts.concat(), "\n\n").to_string())
+}
+
+#[derive(Debug, Logos, PartialEq)]
+#[logos(error = ThisCantHappen)]
+enum LinkToken {
+    #[regex("[\\w]+")]
+    Word,
+    #[regex("[^\\w]+")]
+    NonWord,
+}
+
+fn make_link(header: &str) -> String {
+    const SEPARATOR: &str = "-";
+    use LinkToken::*;
+    let mut parts = vec!["^"];
+    for (token, span) in LinkToken::lexer(header).spanned() {
+        parts.push(if token.unwrap() == Word { &header[span] } else { "-" });
+    }
+    if parts.len() >= 2 && parts[1] == SEPARATOR {
+        parts[1] = "";
+    }
+    if let Some(last) = parts.last() {
+        if *last == SEPARATOR {
+            parts.pop();
+        }
+    }
+    parts.concat().to_lowercase()
+}
+
+fn dice_code(name: &str, link: &str) -> String {
+    ["\n`dice: [[", name, "#", link, "]]`\n"].concat()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const MINIMAL: &str = "# H\n©";
+
+    #[test]
+    fn a_minimal_content_suffices() {
+        assert!(name_prologue_body(MINIMAL).is_ok());
+    }
+
+    #[test]
+    fn prologue_must_contain_copyright_symbol() {
+        assert!(name_prologue_body("# H\ncopyright\n## IJK").is_err());
+    }
+
+    #[test]
+    fn but_if_there_are_no_subsections_then_copyright_isnt_required() {
+        let read_me = "00 Read Me";
+        let rest = "\nblah diddy blah\n";
+        let contents = ["## ", read_me, "\n", rest].concat();
+        assert_eq!(
+            name_prologue_body(&contents).unwrap(),
+            (read_me.to_string(), rest.to_string(), "")
+        );
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn but_OGL_instead_of_copyright_is_ok() {
+        assert!(name_prologue_body("# H\nOGL\nis not copyright\n----\n## Subhead").is_ok());
+    }
+
+    #[test]
+    fn name_prologue_body_does() {
+        // returns file name, prologue, and body
+        let input = "# Owlbear \nThanks\n©\nfoo\n©\nbar\n## Barred Owl";
+        let fname = "Owlbear".to_owned();
+        let prolog = "©\n©\n".to_owned();
+        let body = "\n## Barred Owl";
+        assert_eq!(name_prologue_body(input).unwrap(), (fname, prolog, body));
+    }
+
+    #[test]
+    fn make_link_result_starts_with_newline_and_hat() {
+        assert_eq!(make_link(""), "^");
+    }
+
+    #[test]
+    fn make_link_trims_cruft_and_lowercases() {
+        assert_eq!(make_link("\n@$#$@how%^&^&%NOW-you--------COW-------"), "^how-now-you-cow");
+    }
+
+    #[test]
+    fn dice_code_inserts_name_and_link_into_a_code_template() {
+        let expected = "\n`dice: [[A#B]]`\n";
+        assert_eq!(dice_code("A", "B"), expected);
+    }
+
+    const NAME: &str = "A File Name";
+    #[test]
+    fn parse_requires_nonempty_content_to_begin_with_a_newline() {
+        let bad_content = "How\nnow, brown cow?\n";
+        assert!(parse(NAME, bad_content).is_err());
+    }
+
+    fn parz(contents: &str) -> String {
+        static PARAGRAPH: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\n\n+").unwrap());
+        let parsed = parse(NAME, contents).unwrap();
+        PARAGRAPH.replace_all(&parsed, "¶").to_string()
+    }
+
+    #[test]
+    fn if_entire_content_is_vanilla_then_parse_returns_it_unchanged() {
+        let expected = "\nHow\nnow, brown cow?\n";
+        assert_eq!(parz(expected), expected);
+    }
+
+    #[test]
+    fn heading_followed_by_vanilla_does_not_introduce_a_paragraph() {
+        let expected = "\n## Head\nVanilla";
+        assert_eq!(parz(expected), expected);
+    }
+
+    #[test]
+    fn parse_adds_dice_rolling_code_before_and_link_after_lists() {
+        let input = "\n## Random List\n1. Foo\n2. Baz";
+        let expected =
+            format!("\n## Random List¶`dice: [[{NAME}#^random-list]]`¶1. Foo\n2. Baz¶^random-list");
+        let input = [input, "\nCat Dog"].concat();
+        let expected = [&expected, "¶Cat Dog"].concat();
+        assert_eq!(parz(&input), expected);
+    }
+
+    #[test]
+    fn added_material_is_preceeded_and_followed_by_paragraphs() {
+        let before = ["\n## X", "\n## X\ntext"];
+        let after = ["## Y", "text", ""];
+        let list = "1. a\n2. b";
+        let link = "^x";
+        let code = format!("`dice: [[{NAME}#{link}]]`");
+        for b4 in before {
+            for aft in after {
+                let input = [b4, "\n", list, "\n", aft].concat();
+                let expected = [b4, "¶", &code, "¶", list, "¶", link, "¶", aft].concat();
+                assert_eq!(parz(&input), expected);
+            }
+        }
+    }
+
+    #[test]
+    fn we_add_a_link_after_a_list_that_ends_the_file_even_if_it_doesnt_end_with_a_newline() {
+        let input = "\n## Subhead\n1. Foo\n2. Baz";
+        let expected = format!("\n## Subhead¶`dice: [[{NAME}#^subhead]]`¶1. Foo\n2. Baz¶^subhead¶");
+        assert_eq!(parz(input), expected);
+    }
+
+    #[test]
+    fn check_bad_parse_regression() {
+        const WEIRD: &str = "\n\n1. T\n";
+        let link = "^START";
+        let code = format!("`dice: [[{NAME}#{link}]]`");
+        let expected = ["¶", &code, "¶", "1. T", "¶", link, "¶"].concat();
+        assert_eq!(parz(WEIRD), expected);
+    }
+}
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod test_embedded_file_name {
+    use super::*;
+    // The input must be a line with a Markdown header. The header marker (#, ##, etc) is
+    // trimed, as is the '20 Things #' that sometimes follows the marker. The result is then
+    // trimmed of white space.
+    //
+
+    #[test]
+    fn must_be_a_markdown_header() {
+        assert!(embedded_file_name(" # Too Late").is_err());
+    }
+
+    #[test]
+    fn trims_header_marker_and_whitespace() {
+        assert_eq!(embedded_file_name("#  99 Bottles\t\n").unwrap(), "99 Bottles");
+    }
+
+    #[test]
+    fn trims_20_things_prefix() {
+        // Some of the Raging Swan headers begin for file n begin with '20 Things #n:'.
+        // We trim the '20 Things #' and the colon.
+        assert_eq!(embedded_file_name("# 20 Things #99: Bottles\n").unwrap(), "99 Bottles");
+    }
+
+    #[test]
+    fn embedded_file_name_removes_colon_everywhere() {
+        assert_eq!(embedded_file_name("# 88: Mottles\n").unwrap(), "88 Mottles".to_string());
+    }
+
+    #[test]
+    fn markdown_can_be_header_2_etc() {
+        for octo in ["#", "##", "####"] {
+            let header = format!("{octo} 99 Bottles");
+            assert_eq!(embedded_file_name(&header).unwrap(), "99 Bottles");
+        }
+    }
+
+    #[test]
+    fn tries_to_find_a_better_name_than_Name() {
+        let contents = "# Name\nWhee!\nStuff#00: Better Name. ©";
+        assert_eq!(embedded_file_name(contents).unwrap(), "Better Name");
+    }
+}
