84f852c822d56fcfe3d32211021a7baa9159404d
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2025-02-25 11:46:02 -0500

    Prepare for release
    * Add license files
    * Flesh out the -h and --help flags
    * Expand README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-The *[Dread Thingonomicon](https://www.ragingswanpress.com/system-neutral/the-dread-thingonomicon)* and *[Dread Laironomicon](https://www.ragingswanpress.com/system-neutral/dread-laironomicon)* books from [Raging Swan Press](https://www.ragingswanpress.com/) have hundreds of random tables. The `dreadnom` command allows you to adjust the Markdown files distributed with each book so you that can select a random entry from any table with the click of a mouse, when you use Obsidian and the Dice Roller plugin.
+The _[Dread Thingonomicon](https://www.ragingswanpress.com/system-neutral/the-dread-thingonomicon)_ and _[Dread Laironomicon](https://www.ragingswanpress.com/system-neutral/dread-laironomicon)_ books from [Raging Swan Press](https://www.ragingswanpress.com/) have hundreds of random tables. The `dreadnom` command allows you to adjust the Markdown files distributed with each book so you that can select a random entry from any table with the click of a mouse, when you use [Obsidian](https://obsidian.md/) and the [Dice Roller](https://plugins.javalent.com/Dice+Roller/Dice+Roller) plugin.
+
+Examples:
+
+- `dreadnom DT_TextFiles.zip MyObsidianVault/Thingonomicon`  
+  Creates the `Thingonomicom` folder in `MyObsidianVault` and populates it with the notes in `DT_TextFiles.zip`. Every random table in the notes will have one item, chosen randomly, displayed in bold immediately at the head of the table. Click on the bold item to switch to chose another item at random.
+- `dreadnom Dread_Laironomicon_Text_Archive.zip MyObsidianVault/Laironomicon`  
+  As above but for the Laironomicon.
--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,55 @@
     unused_qualifications,
     unused_must_use
 )]
-#![allow(
-    clippy::items_after_statements,
-    clippy::missing_errors_doc,
-    clippy::semicolon_if_nothing_returned,
-    clippy::struct_excessive_bools,
-    clippy::let_underscore_untyped
-)]
+#![allow(clippy::doc_markdown)]
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_variables))]
 
 use anyhow::Result;
 use camino::Utf8PathBuf;
-use clap::Parser;
+use clap::{ColorChoice, Parser, builder::styling};
+use color_print::cstr;
 
 use dreadnom::reformat_for_obsidian;
 
+const STYLES: styling::Styles = styling::Styles::styled()
+    .header(styling::AnsiColor::Green.on_default().bold())
+    .usage(styling::AnsiColor::Green.on_default().bold())
+    .literal(styling::AnsiColor::Blue.on_default().bold())
+    .placeholder(styling::AnsiColor::Cyan.on_default());
+
+const ABOUT: &str = "Dice rolling in Obsidian for the Dread Thingonomion/Laironomicon";
+const SHORT: &str = concat!(
+    cstr!("\nUse <bold,blue>dreadnom</> to adjust your purchased copy of Raging Swan's"),
+    " Dread Thingonomicon/Laironomicon for digital dice rolling in Obsidian"
+);
+const LONG: &str = concat!(
+    cstr!("\nUse <bold,blue>dreadnom</> and your purchased copy of"),
+    " Raging Swan's Dread Thingonomicon or Dread Laironomicon to create",
+    " a folder in your Obsidian vault where you can click each random table",
+    " to choose one of the random entries.\n\n",
+    cstr!(r#"See the "00 READ ME" note in the Obsidian folder <bold,blue>dreadnom</>"#),
+    " creates for information about the Dice Roller plugin you'll need.",
+);
 #[derive(Parser)]
+#[command(
+    arg_required_else_help=true,
+    version,
+    color=ColorChoice::Auto,
+    about = ABOUT,
+    next_line_help = true,
+    styles=STYLES,
+    after_help=SHORT,
+    after_long_help=LONG,
+)]
 struct Args {
+    /// A Zip file — usually DT_TextFiles.zip for the Dread Thingonomicon
+    /// or Dread_Laironomicon_Text_Archive.zip for the Dread Laironomicon.
+    ///
+    /// OR — a directory into which you've unzipped the contents of one of
+    /// the above
     source: Utf8PathBuf,
+    /// A folder inside your Obsidian vault. The folder need not currently
+    /// exist. If it does, it must contain only Markdown (.md) files
     obsidian: Utf8PathBuf,
 }
 
